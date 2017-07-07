36432ab7da981d18bda9e7de759e311952e7588f
marcelaOrellana <marcelapaz.1985@gmail.com>
2017-07-07 14:11:23 -0400

    se ven nombres y imagenes de 10 pokemones

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ $(document).ready(function($) {
 			url : 'http://pokeapi.co/api/v2/pokemon/',
 			type : 'GET',
 			datatype : 'json',
-			data : {'limit': '20'}
+			data : {'limit': '10'}
 		})
 		.done(function(respuesta){
 			console.log("successe");
-			console.log(respuesta);
+			//console.log(respuesta);
 			nombresPK(respuesta);
 		})
 		.fail(function(){
@@ -23,6 +23,9 @@ $(document).ready(function($) {
 				var nombre = val.name;
 				var urlPK = val.url;
 
+				var pokeNombre = $("<p>").text(nombre);
+		        	        
+
 				$.ajax({
 					url : urlPK,
 					type : 'GET',
@@ -31,28 +34,38 @@ $(document).ready(function($) {
 				})
 				.done(function(respuesta){
 					console.log("successe");
-					console.log(respuesta);
-					
+					//console.log(respuesta);
+					imagenPK(respuesta);
 				})
 				.fail(function(){
 					console.log("error");
 				})
 
-				console.log(nombre);
-				var numero1 = val.url.charAt(33);
-				var numero2 = val.url.charAt(34);
-				if(numero2 == "/"){
-		        	var num = numero1;
-		        }else{
-		        	var num = numero1.concat(numero2);
-        		}
-				console.log(num);
+				function imagenPK(datos){
 
-				//$(".lista").append("<div id='pokeId" + num + "'></div>")
-				var foto = $("<img>").attr("src","http://pokeapi.co/media/img/" + num + ".png");
-				var pokeNombre = $("<p>").text(nombre);
-		        $(".lista").append(foto);
-		        $('.lista').append(pokeNombre);
+					var idPK = datos.id;
+
+					//console.log("id: " + idPK);
+					var foto = $("<img>").attr("src","http://pokeapi.co/media/img/" + idPK + ".png");
+					var cadaPK = $("<div>");
+					cadaPK.attr('data-toggle', 'modal');
+					cadaPK.attr('data-target', '#myModal' + idPK);
+					cadaPK.append(foto);
+					cadaPK.append(pokeNombre);
+					var modalPK = $('<div class="modal fade" id="myModal' + idPK + '" role="dialog"><div class="modal-dialog"><!-- Modal content--><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button><h4 class="modal-title">Modal Header</h4></div><div class="modal-body"><p>Some text in the modal.</p></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div></div>');
+					$(".modal").on("shown.bs.modal", function () {
+                	if ($(".modal-backdrop").length > 1) {
+                    	$(".modal-backdrop").not(':first').remove();
+                    	$("body").attr('style', 'padding-right: 0');
+                	}
+            })
+					cadaPK.append(modalPK);
+					$(".lista").append(cadaPK);
+					
+					
+				}
+				
+				
 			});
 
 			
