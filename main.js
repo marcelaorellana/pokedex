93deba6ebeaa937408cf3cd100bc430ab8716d6b
marcelaorellana <marcelapaz.1985@gmail.com>
2017-07-07 20:23:18 -0400

    modal con imagen, habilidades y tipo

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ $(document).ready(function($) {
 				})
 				.done(function(respuesta){
 					console.log("successe");
-					//console.log(respuesta);
+					console.log(respuesta);
 					imagenPK(respuesta);
 				})
 				.fail(function(){
@@ -44,21 +44,33 @@ $(document).ready(function($) {
 				function imagenPK(datos){
 
 					var idPK = datos.id;
-
-					//console.log("id: " + idPK);
+					var nombrePokemon = datos.name;
+					var tipo = datos.types;
+					var abilidades = datos.abilities;
+					var tipoCadena = "";
+					var tipoHabilidad = "";
+					for(i=0; i<tipo.length; i++){
+            			//console.log(tipo[i].type.name);
+            			tipoCadena += "<p>" + tipo[i].type.name + "</p>";
+            		}
+            		for(i=0; i<tipo.length; i++){
+            			console.log(abilidades[i].ability.name);
+            			tipoHabilidad += "<p>" + abilidades[i].ability.name + "</p>";
+            		}
 					var foto = $("<img>").attr("src","http://pokeapi.co/media/img/" + idPK + ".png");
 					var cadaPK = $("<div>");
 					cadaPK.attr('data-toggle', 'modal');
 					cadaPK.attr('data-target', '#myModal' + idPK);
 					cadaPK.append(foto);
 					cadaPK.append(pokeNombre);
-					var modalPK = $('<div class="modal fade" id="myModal' + idPK + '" role="dialog"><div class="modal-dialog"><!-- Modal content--><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button><h4 class="modal-title">Modal Header</h4></div><div class="modal-body"><p>Some text in the modal.</p></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div></div>');
+					var modalPK = $('<div class="modal fade" id="myModal' + idPK + '" role="dialog"><div class="modal-dialog"><!-- Modal content--><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button><h4 class="modal-title">' + nombrePokemon + '</h4></div><div class="modal-body"> <img src="http://pokeapi.co/media/img/'+idPK+ '.png" alt=""> <div class="tipoClass">'+ tipoCadena + '</div> </div><div class="abilidadClass">'+ tipoHabilidad + '</div> </div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div></div>');
 					$(".modal").on("shown.bs.modal", function () {
                 	if ($(".modal-backdrop").length > 1) {
                     	$(".modal-backdrop").not(':first').remove();
                     	$("body").attr('style', 'padding-right: 0');
                 	}
-            })
+            		});
+            		
 					cadaPK.append(modalPK);
 					$(".lista").append(cadaPK);
 					
