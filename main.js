aa6246f3487020b708e42c81391c1e5350060170
marcelaorellana <marcelapaz.1985@gmail.com>
2017-07-07 22:22:49 -0400

    lo más lindo del mundo

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ $(document).ready(function($) {
 			url : 'http://pokeapi.co/api/v2/pokemon/',
 			type : 'GET',
 			datatype : 'json',
-			data : {'limit': '20'}
+			data : {'limit': '21'}
 		})
 		.done(function(respuesta){
 			console.log("successe");
@@ -51,23 +51,28 @@ $(document).ready(function($) {
 					var tipoHabilidad = "";
 					for(i=0; i<tipo.length; i++){
             			//console.log(tipo[i].type.name);
-            			tipoCadena += "<p>" + tipo[i].type.name + "</p>";
+            			//tipoCadena += "<p>" + tipo[i].type.name + "</p>";
+            			tipoCadena += "<li><p>" + tipo[i].type.name + "</p></li>";
             		}
             		for(i=0; i<tipo.length; i++){
-            			console.log(abilidades[i].ability.name);
-            			tipoHabilidad += "<p>" + abilidades[i].ability.name + "</p>";
+            			//console.log(abilidades[i].ability.name);
+            			tipoHabilidad += "<li><p>" + abilidades[i].ability.name + "</p></li>";
             		}
 					var foto = $("<img>").attr("src","http://pokeapi.co/media/img/" + idPK + ".png");
+					var spacioFoto =$("<div>");
 					var cadaPK = $("<div>");
+					spacioFoto.attr("class", "spacioFoto");
+					cadaPK.attr("class", "cajas");
 					cadaPK.attr('data-toggle', 'modal');
 					cadaPK.attr('data-target', '#myModal' + idPK);
-					cadaPK.append(foto);
+					spacioFoto.append(foto);
+					cadaPK.append(spacioFoto);
 					cadaPK.append(pokeNombre);
-					var modalPK = $('<div class="modal fade" id="myModal' + idPK + '" role="dialog"><div class="modal-dialog"><!-- Modal content--><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button><h4 class="modal-title">' + nombrePokemon + '</h4></div><div class="modal-body"> <img src="http://pokeapi.co/media/img/'+idPK+ '.png" alt=""> <div class="tipoClass">'+ tipoCadena + '</div> </div><div class="abilidadClass">'+ tipoHabilidad + '</div> </div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div></div>');
+					var modalPK = $('<div class="modal fade" id="myModal' + idPK + '" role="dialog"><div class="modal-dialog"><!-- Modal content--><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button><h1 class="modal-title">' + nombrePokemon + '</h1></div><div class="modal-body"> <img src="http://pokeapi.co/media/img/'+idPK+ '.png" alt="" class="imagModal"> <div class="tipoClass"><h3>Tipo de Pokemón</h3><ul>'+ tipoCadena + '</ul></div> <div class="abilidadClass"><h3>Habilidades</h3><ul>'+ tipoHabilidad + '</ul></div></div> </div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div></div>');
 					$(".modal").on("shown.bs.modal", function () {
                 	if ($(".modal-backdrop").length > 1) {
                     	$(".modal-backdrop").not(':first').remove();
-                    	$("body").attr('style', 'padding-right: 0');
+                    	$("body").attr('style', 'padding-right: 0'); 
                 	}
             		});
             		
@@ -99,7 +104,9 @@ $(document).ready(function($) {
 
 		$(".antes").on("click", function(e){
 					$(".lista").empty();
-					paginacion -= 20;
+					if(paginacion > 0){
+						paginacion -= 20;
+					}
 					$.ajax({
 						url : 'http://pokeapi.co/api/v2/pokemon/',
 						type : 'GET',
